06f0b8046bf90e7548b648bdfeb1f3c40a75e41b
komil <komashuxratov@gmail.com>
2024-01-23 20:10:31 +0500

    changed

--- app/src/main/java/uz/sardor/komilcars/SplashFragment.kt
+++ app/src/main/java/uz/sardor/komilcars/SplashFragment.kt
@@ -5,16 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
+import androidx.navigation.fragment.findNavController
+import uz.sardor.komilcars.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        val binding = FragmentSplashBinding.inflate(inflater,container,false)
+
+        binding.mercedes.setOnClickListener {
+        }
+        binding.bmw.setOnClickListener{
+        }
+
+        binding.signIn.setOnClickListener {
+            findNavController().navigate(R.id.action_splashFragment2_to_homeFragment)
+        }
+        return binding.root
     }
 
 
