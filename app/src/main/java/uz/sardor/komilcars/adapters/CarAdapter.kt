c12fae7a08d546f9311d72db8ff7d88399f1fec5
= <=>
2024-01-24 11:26:55 +0500

    Sardorchik

--- app/src/main/java/uz/sardor/komilcars/adapters/CarAdapter.kt
+++ app/src/main/java/uz/sardor/komilcars/adapters/CarAdapter.kt
@@ -1,77 +1,77 @@
-package uz.sardor.komilcars.adapters
-
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
-import uz.sardor.komilcars.R
-import uz.sardor.komilcars.model.Car
-
-class CarAdapter(var list1:MutableList<Car>, var carInterface:CarInterface) :RecyclerView.Adapter<CarAdapter.CarHolder>(){
-
-     class CarHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-         val photo:ImageView = itemView.findViewById(R.id.car_photo)
-         val company_name:TextView = itemView.findViewById(R.id.company_name)
-         val model_name:TextView = itemView.findViewById(R.id.model_name)
-         val price: TextView = itemView.findViewById(R.id.price_button)
-         val liked:ImageView = itemView.findViewById(R.id.like)
-     }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarHolder {
-        val holder = CarHolder(LayoutInflater.from(parent.context).inflate(R.layout.offer_item,parent,false))
-        return holder
-    }
-
-    override fun getItemCount(): Int {
-        return list1.size
-    }
-
-    override fun onBindViewHolder(holder: CarHolder, position: Int) {
-        var item1 = list1[position]
-        holder.photo.setImageResource(item1.photo)
-        holder.company_name.text = item1.company_name
-        holder.model_name.text = item1.model_name
-        holder.price.text = item1.price
-        holder.liked.setImageResource(item1.liked)
-        holder.itemView.setOnClickListener {
-            carInterface.OnItemClick(item1)
-        }
-        var a = 0
-        holder.liked.setOnClickListener {
-                if (a == 0){
-                    holder.liked.setImageResource(R.drawable.like_filled)
-                        a = 1
-                }
-
-                }
-        holder.liked.setOnClickListener {
-                if (a == 1) {
-                    holder.liked.setOnClickListener {
-                        holder.liked.setImageResource(R.drawable.like_unfilled)
-                        a = 0
-                    }
-
-                }
-            else{
-                    holder.liked.setImageResource(R.drawable.like_filled)
-                }
-//            holder.liked.setOnClickListener {
-//                holder.liked.setImageResource(R.drawable.like_filled)
-//                a = 1
+//package uz.sardor.komilcars.adapters
+//
+//import android.view.LayoutInflater
+//import android.view.View
+//import android.view.ViewGroup
+//import android.widget.ImageView
+//import android.widget.TextView
+//import androidx.recyclerview.widget.RecyclerView
+//import uz.sardor.komilcars.R
+//import uz.sardor.komilcars.model.Car
+//
+//class CarAdapter(var list1:MutableList<Car>, var carInterface:CarInterface) :RecyclerView.Adapter<CarAdapter.CarHolder>(){
+//
+//     class CarHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+////         val photo:ImageView = itemView.findViewById(R.id.car_photo)
+////         val company_name:TextView = itemView.findViewById(R.id.company_name)
+////         val model_name:TextView = itemView.findViewById(R.id.model_name)
+////         val price: TextView = itemView.findViewById(R.id.price_button)
+////         val liked:ImageView = itemView.findViewById(R.id.like)
+//     }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CarHolder {
+////        val holder = CarHolder(LayoutInflater.from(parent.context).inflate(R.layout.offer_item,parent,false))
+//        return holder
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return list1.size
+//    }
+//
+//    override fun onBindViewHolder(holder: CarHolder, position: Int) {
+//        var item1 = list1[position]
+//        holder.photo.setImageResource(item1.photo)
+//        holder.company_name.text = item1.company_name
+//        holder.model_name.text = item1.model_name
+//        holder.price.text = item1.price
+//        holder.liked.setImageResource(item1.liked)
+//        holder.itemView.setOnClickListener {
+//            carInterface.OnItemClick(item1)
+//        }
+//        var a = 0
+//        holder.liked.setOnClickListener {
+//                if (a == 0){
+//                    holder.liked.setImageResource(R.drawable.like_filled)
+//                        a = 1
+//                }
+//
+//                }
+//        holder.liked.setOnClickListener {
+//                if (a == 1) {
+//                    holder.liked.setOnClickListener {
+//                        holder.liked.setImageResource(R.drawable.like_unfilled)
+//                        a = 0
+//                    }
+//
+//                }
+//            else{
+//                    holder.liked.setImageResource(R.drawable.like_filled)
+//                }
+////            holder.liked.setOnClickListener {
+////                holder.liked.setImageResource(R.drawable.like_filled)
+////                a = 1
+////            }
+//
 //            }
-
-            }
-
-
-
-        }
-    interface CarInterface{
-        fun OnItemClick(car: Car)
-
-
-    }
-
-    }
-
+//
+//
+//
+//        }
+//    interface CarInterface{
+//        fun OnItemClick(car: Car)
+//
+//
+//    }
+//
+//    }
+//
--- app/src/main/java/uz/sardor/komilcars/fragments/SplashFragment.kt
+++ app/src/main/java/uz/sardor/komilcars/fragments/SplashFragment.kt
@@ -1,32 +1,52 @@
 package uz.sardor.komilcars.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import uz.sardor.komilcars.R
 import uz.sardor.komilcars.databinding.FragmentSplashBinding
+
 
 class SplashFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
+
+
         val binding = FragmentSplashBinding.inflate(inflater, container, false)
 
-        binding.merc.setOnClickListener {
-        }
-        binding.bmw.setOnClickListener{
-        }
 
-        binding.signIn.setOnClickListener {
-            findNavController().navigate(R.id.action_splashFragment2_to_signInFragment)
-        }
-        binding.signUp.setOnClickListener {
-          findNavController().navigate(R.id.action_splashFragment2_to_signUpFragment)
-         }
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            findNavController().navigate(uz.sardor.komilcars.R.id.action_splashFragment2_to_signInFragment)
+        }, 3800)
+        return binding.root
+
+
+//
+//        binding.merc.setOnClickListener {
+//        }
+//        binding.bmw.setOnClickListener{
+//        }
+//
+//        binding.signIn.setOnClickListener {
+//            findNavController().navigate(R.id.action_splashFragment2_to_signInFragment)
+//        }
+//        binding.signUp.setOnClickListener {
+//          findNavController().navigate(R.id.action_splashFragment2_to_signUpFragment)
+//         }
         return binding.root
     }
 
